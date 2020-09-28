ebba9d0de238d5ce8d45b968b4657ef110676d40
Margarida Silva <t-ms@platforme.com>
2020-09-28 08:34:29 +0100

    Revert "Review endpoints methods"
    This reverts commit e0c8edd6992ab077ea86799795ff91e1f8f572d0.

--- js/base.js
+++ js/base.js
@@ -1,10 +1,9 @@
 import { API as BaseAPI, mix, load, conf } from "yonius";
 import { OrderAPI } from "./order";
-import { ReviewAPI } from "./review";
 
 const TEMPLATING_SERVICE_URL = "http://localhost:3000/";
 
-export class API extends mix(BaseAPI).with(OrderAPI, ReviewAPI) {
+export class API extends mix(BaseAPI).with(OrderAPI) {
     constructor(kwargs = {}) {
         super(kwargs);
         this.baseUrl = conf("TEMPLATING_SERVICE_URL", TEMPLATING_SERVICE_URL);

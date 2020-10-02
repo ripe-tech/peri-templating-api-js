31b562ef99f6618f5e0a6af612643335ef61dcee
Margarida Silva <t-ms@platforme.com>
2020-10-02 09:46:34 +0100

    Update review with changes in yonius

--- js/review.js
+++ js/review.js
@@ -2,26 +2,23 @@ export const ReviewAPI = superclass =>
     class extends superclass {
         async getReviewCreated(event) {
             const url = this.baseUrl + "review";
-            const contents = await this.post(url, {
+            return await this.post(url, {
                 dataJ: event
             });
-            return await contents.text();
         }
 
         async getReviewApproved(event) {
             const url = this.baseUrl + "review/approved";
-            const contents = await this.post(url, {
+            return await this.post(url, {
                 dataJ: event
             });
-            return await contents.text();
         }
 
         async getReviewRejected(event) {
             const url = this.baseUrl + "review/rejected";
-            const contents = await this.post(url, {
+            return await this.post(url, {
                 dataJ: event
             });
-            return await contents.text();
         }
     };
 

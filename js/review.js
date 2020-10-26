8cda8164a045b9e04f830dcdcbc91a450c3c1389
Margarida Silva <ms@platforme.com>
2020-10-26 11:48:33 +0000

    Use event name to get URL instead of one method for each URL

--- js/review.js
+++ js/review.js
@@ -1,42 +1,12 @@
 export const ReviewAPI = superclass =>
     class extends superclass {
-        async createReviewOwner(event) {
-            const url = this.baseUrl + "review/opened-owner";
-            return await this.post(url, {
-                dataJ: event
-            });
-        }
+        async getReview(event) {
+            const parts = event.name.split(".");
+            const state = parts.length === 3 ? parts[1] : null;
+            const role = parts.length === 3 ? parts[2] : parts[1];
 
-        async createReviewReviewer(event) {
-            const url = this.baseUrl + "review/opened-reviewer";
-            return await this.post(url, {
-                dataJ: event
-            });
-        }
-
-        async createApprovedReviewOwner(event) {
-            const url = this.baseUrl + "review/approved-owner";
-            return await this.post(url, {
-                dataJ: event
-            });
-        }
-
-        async createApprovedReviewReviewer(event) {
-            const url = this.baseUrl + "review/approved-reviewer";
-            return await this.post(url, {
-                dataJ: event
-            });
-        }
-
-        async createRejectedReviewOwner(event) {
-            const url = this.baseUrl + "review/rejected-owner";
-            return await this.post(url, {
-                dataJ: event
-            });
-        }
-
-        async createRejectedReviewReviewer(event) {
-            const url = this.baseUrl + "review/rejected-reviewer";
+            const templateName = `${state || "opened"}-${role}`;
+            const url = this.baseUrl + `review/${templateName}`;
             return await this.post(url, {
                 dataJ: event
             });

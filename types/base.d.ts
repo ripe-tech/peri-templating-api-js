667807edb43cbde55e8b1a4e940850bb1ff2ab34
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-02-18 12:48:57 +0000

    feat: add types and build method

--- types/base.d.ts
+++ types/base.d.ts
@@ -3,8 +3,28 @@ import { ReviewAPI } from "./review";
 
 export interface API extends OrderAPI, ReviewAPI {}
 
+export type BuildOptions = {
+    headers?: Record<string, unknown>;
+    kwargs?: Record<string, unknown>;
+    params?: Record<string, unknown>;
+    handle?: boolean;
+    data?: object,
+    dataJ?: JSON,
+    dataM?: object,
+}
+
+export enum BuildMethod {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    PATCH = "PATCH",
+    OPTIONS = "OPTIONS"
+}
+
 export declare class API implements API {
     constructor(kwargs?: object);
     load(): Promise<void>;
+    build(method: BuildMethod, url: string, options?: BuildOptions)
     info(): Promise<object>;
 }

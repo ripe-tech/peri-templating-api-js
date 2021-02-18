a40ae0e95e42798a1b6f70e37a180437dfa20d42
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-02-18 15:28:30 +0000

    feat: remove BuildOptions and BuildMethod

--- types/base.d.ts
+++ types/base.d.ts
@@ -1,25 +1,5 @@
 import { API as BaseAPI } from "yonius";
 
-export type BuildOptions = {
-    headers?: Record<string, unknown>;
-    kwargs?: Record<string, unknown>;
-    params?: Record<string, unknown>;
-    handle?: boolean;
-    data?: object,
-    dataJ?: JSON,
-    dataM?: object,
-}
-
-export enum BuildMethod {
-    GET = "GET",
-    POST = "POST",
-    PUT = "PUT",
-    DELETE = "DELETE",
-    PATCH = "PATCH",
-    OPTIONS = "OPTIONS"
-}
-
-
 export declare class API extends BaseAPI {
     constructor(kwargs?: object);
     load(): Promise<void>;
